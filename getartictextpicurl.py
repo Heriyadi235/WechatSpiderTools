72b2c5e274cfe25dca00e7ff791376280f6868a1
陈禹西 <30655304+Heriyadi235@users.noreply.github.com>
2018-02-23 19:11:59 +0800

    fix bugs

--- getartictextpicurl.py
+++ getartictextpicurl.py
@@ -11,35 +11,56 @@ def html2text(arurl):
     res = request.urlopen(arurl)
     htmlstr = res.read()
     #print(htmlstr)
-    title = "untitled"
-    info = "noinfo"
-    text = "nope"
     soup = BeautifulSoup(htmlstr, "lxml")
     title = soup.title.get_text('\n')
+    #print(title)
     info = soup.em.get_text('')
-    text = soup.section.get_text('\n')
+    #print(info)
+    text=''
+    #text = soup.section.get_text('\n')#第一个BUG
+    #改抓这个标签试试<div class="rich_media_content " id="js_content">
+    a=soup.find_all('div',{'class':'rich_media_content '})
+    #a=soup.find_all('section')
+    for i in a:
+        text = text + i.get_text('\n')
+    '''
+    for content in soup.find_all(name = 'div'):
+        if content.get('class')==['rich_media_content ']:
+            text = text + content.get_text() + '\n'#在txt中，要用"\r\n"才能换行，get_text()用来获取标签内里面的文本内容，"strip=True"能去除文本内容前后多余的空格
+    '''
+    #textlist = soup.find_all(name='section')
+    #for each in textlist:
+        #print(each.get('data-src'))
+        #print(each.get('data-src'))
+        #f.write(each.get('data-src')+ '\n')
+    #print(text)
     #r=''
+    #print('text:' + text)
     filename = re.sub('[’!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~]+','',title)
-    with open('E:\\spidersour\\' + info +' ' + filename + '.txt', 'w+', encoding='utf-8') as f:
+    #print('手动断点')
+    #with open('E:\\spidersour\\' + info + filename + '.txt', 'w+', encoding='utf-8') as f:
+    with open('E:\\spidersour\\' + info +' '+ filename + '.txt', 'w+', encoding='utf-8') as f:
         f.write(title + '\n' )
         f.write(info + '\n' )
         f.write(text + '\n' )
+        #f.write('这里是正文\n' )
         f.write('图片链接' + '\n')
+        #f.write(arurl + '\n' )
+        #图片这要重写
         imglist = soup.find_all(name='img')
-        i = 0
-        for each in imglist:
-            if i < 2:
-                i += 1
-                continue
+        for i in imglist:
                 #print(each.get('data-src'))
-            f.write(each.get('data-src')+ '\n')
-            i += 1
-            if i > 4:
-                break
+            #f.write(i['data-src']+ '\n')
+            f.write(str(i.get('data-src'))+ '\n')
+                    #i += 1
+                #if i > 4:
+                    #break
         print('图片爬取完毕')
+        f.write('原文链接' + '\n')
+        f.write(arurl + '\n')
         f.close()
     #print(info)
-    print(title+'已保存到本地')
+    print('文章' + title + ' 已保存到本地')
     print('==========================================================================================================')
 
     #print(soup)
@@ -51,7 +72,7 @@ if __name__=='__main__':
             print(line)
             arurl = line
             html2text(arurl)
-            time.sleep(3)
+            time.sleep(1)
         print("爬取完成")
     except Exception as e:
         print(str(e))
